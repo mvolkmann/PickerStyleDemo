3387f4b42875956f89a38bfaebf0ac2f34789f9d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-20 09:25:31 -0500

    Initial Commit

---
+++ PickerStyleDemo/PickerStyleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerStyleDemoApp.swift
+//  PickerStyleDemo
+//
+//  Created by Mark Volkmann on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct PickerStyleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
