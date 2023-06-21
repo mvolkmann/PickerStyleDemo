1ef55289e00659afb03117295b9aba025fc8f67b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-21 12:32:33 -0500

    more on palette-style Picker

--- PickerStyleDemo/ContentView.swift
+++ PickerStyleDemo/ContentView.swift
@@ -19,17 +19,31 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Picker("Status", selection: $selectedStatus) {
-                ForEach(statuses) { status in
-                    // This is not rendering as expected! See
-                    // https://developer.apple.com/forums/thread/732208.
-                    Label(status.title, systemImage: "tag")
-                        .tint(status.color)
-                        .tag(status)
+            Menu(selectedStatus.title) {
+                // Tapping the menu causes this Picker to appear.
+                // Selecting an option highlights it,
+                // but the Picker remains rendered.
+                // Users must tap outside the Picker to dismiss it.
+                // TODO: Is there a way to dismiss it programmatically?
+                Picker("Select a status.", selection: $selectedStatus) {
+                    ForEach(statuses) { status in
+                        // This is not rendering as expected! See
+                        // https://developer.apple.com/forums/thread/732208.
+                        Label(status.title, systemImage: "tag")
+                            .tint(status.color)
+                            .tag(status)
+                    }
+                }
+                // When the palette style is used outside of a menu,
+                // a segmented picker is rendered.
+                // If that is desired, use the segmented style instead.
+                .pickerStyle(.palette)
+                .paletteSelectionEffect(.symbolVariant(.fill))
+                .onChange(of: selectedStatus) {
+                    // TODO: Is there something you can do here
+                    // TODO: to dismiss the Picker?
                 }
             }
-            .pickerStyle(.palette)
-            .paletteSelectionEffect(.symbolVariant(.fill))
 
             Text("Status is \(selectedStatus.title).")
 
